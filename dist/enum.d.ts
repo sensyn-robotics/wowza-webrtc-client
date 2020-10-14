c6b9608972475c0b8650b3a5f47a0940a9085ff7
Han Sen <s-han@sensyn-robotics.com>
2020-10-14 18:55:45 +0900

    fix repack

---
+++ dist/enum.d.ts
@@ -0,0 +1,4 @@
+export declare enum StreamingType {
+    USER_MEDIA = 0,
+    DISPLAY_MEDIA = 1
+}
---
+++ dist/enum.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StreamingType = void 0;
+var StreamingType;
+(function (StreamingType) {
+    StreamingType[StreamingType["USER_MEDIA"] = 0] = "USER_MEDIA";
+    StreamingType[StreamingType["DISPLAY_MEDIA"] = 1] = "DISPLAY_MEDIA";
+})(StreamingType = exports.StreamingType || (exports.StreamingType = {}));
